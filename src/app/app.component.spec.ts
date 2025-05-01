44025d7a1f6f8dda42ad02783395fa6cfbacdfdb
Christian <christian.ramon.dev@gmail.com>
2025-05-01 16:07:34 -0600

    test: update app tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,6 @@
 import { TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
@@ -12,5 +14,19 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
+  });
+
+  it('should render the app-canvas component', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const canvasElement = fixture.debugElement.query(By.css('app-canvas'));
+    expect(canvasElement).toBeTruthy();
+    expect(canvasElement.componentInstance).toBeTruthy();
+  });
+
+  it('should render the app-toolbar component', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const toolbarElement = fixture.debugElement.query(By.css('app-toolbar'));
+    expect(toolbarElement).toBeTruthy();
+    expect(toolbarElement.componentInstance).toBeTruthy();
   });
 });
